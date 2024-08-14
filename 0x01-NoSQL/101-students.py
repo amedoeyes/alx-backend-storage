14c07242c91eef2ef39d2a49f28db5a042eda7a2
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-08-14 04:10:30 +0300

    add 101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""Top students"""
+
+
+def top_students(mongo_collection):
+    """Top students"""
+    students = mongo_collection.aggregate(
+        [
+            {
+                "$project": {
+                    "_id": 1,
+                    "name": 1,
+                    "averageScore": {
+                        "$avg": {"$avg": "$topics.score"},
+                    },
+                    "topics": 1,
+                },
+            },
+            {"$sort": {"averageScore": -1}},
+        ]
+    )
+    return students
